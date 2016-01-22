bbc34b1518b6a325223dc541ec8fb3e628d8376b
wangcui <wangc_os@sari.ac.cn>
2016-01-22 16:29:24 +0800

    add prjs/dojo/gauge demo

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/prjs/orig/')));
+app.use(express.static(path.join(__dirname, '/')));
 
 app.use('/', require('./routes/index'));
+app.use('/dj/gauge', require('./routes/dojo/gauge'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
