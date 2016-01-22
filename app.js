b2fb1943d3e6c21e98393b9c5258d9e88ee6ec4b
wangcui <wangc_os@sari.ac.cn>
2016-01-22 15:52:33 +0800

    mv original welcome page [index.ejs style.css] to prjs/orig

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/')); //set views dir to root dir
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -20,10 +17,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/prjs/orig/')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
